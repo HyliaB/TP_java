6c8e5f8f9f10125fb4c3800b18e07a09def5c1a6
hyliaB <hylia.boudahba@decathlon.com>
2025-02-21 18:01:39 +0100

    fonction equals

--- Pokemon.java
+++ Pokemon.java
@@ -1,35 +1,31 @@
-class Pokemon{
+class Pokemon {
 
-   private int numPokemon;
-   private String especes;
-   private String nomPokemon;
-   private String typePokemon1; 
-   private String typePokemon2; 
-   private int attPokemon;
-   private int defPokemon;
-   private int vitessePokemon;
-   private int pvPokemon; 
+    private int numPokemon;
+    private String especes;
+    private String nomPokemon;
+    private String typePokemon1;
+    private String typePokemon2;
+    private int attPokemon;
+    private int defPokemon;
+    private int vitessePokemon;
+    private int pvPokemon;
 
-    public Pokemon(){
+    public Pokemon() {
         this.numPokemon = 1;
-        this.especes    = Type.getEspece(25);
+        this.especes = Type.getEspece(25);
         this.nomPokemon = "pika_pika";
         this.typePokemon1 = Type.getNomType(3);
         this.typePokemon2 = Type.getNomType(2);
-        this.attPokemon   = 64;
-        this.defPokemon   = 58;
+        this.attPokemon = 64;
+        this.defPokemon = 58;
         this.vitessePokemon = 70;
-        this.pvPokemon      = 62;
+        this.pvPokemon = 62;
     }
-    
 
-
-
-
-
-        public Pokemon(int numPokemon, String especes, String nomPokemon, String typePokemon1, String typePokemon2,
-        int attPokemon, int defPokemon, int vitessePokemon, int pvPokemon) {
+    public Pokemon(int numPokemon, String especes, String nomPokemon, String typePokemon1, String typePokemon2,
+                   int attPokemon, int defPokemon, int vitessePokemon, int pvPokemon) {
         this.numPokemon = numPokemon;
+        this.especes = especes;
         this.nomPokemon = nomPokemon;
         this.typePokemon1 = typePokemon1;
         this.typePokemon2 = typePokemon2;
@@ -37,54 +33,46 @@ class Pokemon{
         this.defPokemon = defPokemon;
         this.vitessePokemon = vitessePokemon;
         this.pvPokemon = pvPokemon;
-}
-
-
-
-
-public String toString() {
-    return "Numéro: " + numPokemon + "\n" +
-           "Espece: " + especes + "\n"+
-           "Nom: " + nomPokemon + "\n" +
-           "Type 1: " + typePokemon1 + "\n" +
-           "Type 2: " + (typePokemon2.isEmpty() ? "ø" : typePokemon2) + "\n" +
-           "PV: " + pvPokemon + "\n" +
-           "Att: " + attPokemon + "\n" +
-           "Def: " + defPokemon + "\n" +
-           "Vit: " + vitessePokemon;
-}
-
-public boolean equals(Pokemon pokemon1 , Pokemon pokemon2){
-    if (pokemon1.numPokemon == pokemon2.numPokemon) && (pokemon1.especes == pokemon2.especes) && (pokemon1.nomPokemon == pokemon2.nomPokemon)
-    && (pokemon1.typePokemon1 == pokemon2.typePokemon1) && (pokemon1.typePokemon2 == pokemon2.typePokemon2) && (pokemon1.attPokemon == pokemon2.attPokemon)
-    && (pokemon1.defPokemon == pokemon2.defPokemon) && (pokemon1.vitessePokemon == pokemon2.vitessePokemon) && (pokemon1.pvPokemon == pokemon2.pvPokemon){
-        return true;
-    }
-    else{
-        return false;
     }
 
+    public String toString() {
+        return "Numéro: " + numPokemon + "\n" +
+                "Espece: " + especes + "\n" +
+                "Nom: " + nomPokemon + "\n" +
+                "Type 1: " + typePokemon1 + "\n" +
+                "Type 2: " + (typePokemon2.isEmpty() ? "ø" : typePokemon2) + "\n" +
+                "PV: " + pvPokemon + "\n" +
+                "Att: " + attPokemon + "\n" +
+                "Def: " + defPokemon + "\n" +
+                "Vit: " + vitessePokemon;
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof Pokemon)) {
+            return false;
+        }
+        Pokemon p = (Pokemon) o;
+        return this.numPokemon == p.numPokemon &&
+                this.especes.equals(p.especes) &&
+                this.nomPokemon.equals(p.nomPokemon) &&
+                this.typePokemon1.equals(p.typePokemon1) &&
+                this.typePokemon2.equals(p.typePokemon2) &&
+                this.attPokemon == p.attPokemon &&
+                this.defPokemon == p.defPokemon &&
+                this.vitessePokemon == p.vitessePokemon &&
+                this.pvPokemon == p.pvPokemon;
+    }
 
 
+    public static void main(String[] args) {
+        Pokemon poke001 = new Pokemon(151, "mew", "maurice", "feu", "sol", 88, 58, 70, 62);
+        Pokemon poke002 = new Pokemon(151, "mew", "hylia", "feu", "sol", 88, 58, 70, 62);
 
+        System.out.println(poke001.equals(poke002));
 
+    }
 
 }
-
-
-
-
-
-public static void main(String[] args){
-    Pokemon poke001 = new Pokemon(151,"mew","maurice","feu","sol",88,58,70,62);
-    System.out.println(poke001);
-
-
-
-
-}
-
-
 
 
 
@@ -114,4 +102,3 @@ public static void main(String[] args){
 
 
     
-}
